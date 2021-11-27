9ceb97285bdd0d6ad14f404da6afcc559065725c
shashiksingh786 <to.shashi7886@hotmail.com>
2021-11-27 23:31:29 +0530

    add

--- HackerRankAngular/src/app/app.module.ts
+++ HackerRankAngular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilterComponent } from './filter/filter.component';
+import { SurveyComponent } from './survey/survey.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterComponent,
+    SurveyComponent
   ],
   imports: [
     BrowserModule,
