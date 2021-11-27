13d210331b1d2866c039de7dfe1a72c158e7acc0
shashiksingh786 <to.shashi7886@hotmail.com>
2021-11-28 00:17:45 +0530

    add filter and survey compaonent with values

--- HackerRankAngular/src/app/app.component.ts
+++ HackerRankAngular/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'HackerRankAngular';
+  statuses = ['All', 'Active', 'Disable'];
+  categories = ['Development', 'Testing', 'Deployment'];
+
+  status = 'status';
+  category = 'category';
+  ngOnInit() {
+
+  }
+
 }
